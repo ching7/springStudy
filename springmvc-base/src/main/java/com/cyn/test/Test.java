51fcf7c6c7a7e0573aaf007d33ca7ab214823888
chenyn22577 <chenyn22577@hundsun.com>
2020-02-19 22:03:20 +0800

    add:springmvc-base-demo

---
+++ springmvc-base/src/main/java/com/cyn/test/Test.java
@@ -0,0 +1,27 @@
+package com.cyn.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+
+/**
+ * 文件描述
+ *
+ * @ProjectName: springStudy
+ * @Package: com.cyn.test
+ * @Description: note
+ * @Author: hspcadmin
+ * @CreateDate: 2019/9/20 16:05
+ * @UpdateUser: hspcadmin
+ * @UpdateDate: 2019/9/20 16:05
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ * <p>
+ **/
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext ac = null;
+        HandlerMapping hm = null;
+        HandlerAdapter ha = null;
+    }
+}
