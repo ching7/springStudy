eac558eeb6a9c21b10c671261af7dc3474417303
chenyn22577 <chenyn22577@hundsun.com>
2020-11-12 16:16:04 +0800

    fix:springcloud2020 eureka init

--- springcloud2020/cloud-consumer-order80/src/main/java/com/cyn/springcloud/controller/OrderController.java
+++ springcloud2020/cloud-consumer-order80/src/main/java/com/cyn/springcloud/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.cyn.springcloud.entities.CommonResult;
 import com.cyn.springcloud.entities.Payment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
