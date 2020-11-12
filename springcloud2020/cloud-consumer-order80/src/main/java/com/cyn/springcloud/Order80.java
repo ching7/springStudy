36f48750c73c990a579866fa035243c1d2f4dfa6
chenyn22577 <chenyn22577@hundsun.com>
2020-11-12 20:03:03 +0800

    add:init springcloud2020 eureka single and cluster

--- springcloud2020/cloud-consumer-order80/src/main/java/com/cyn/springcloud/Order80.java
+++ springcloud2020/cloud-consumer-order80/src/main/java/com/cyn/springcloud/Order80.java
@@ -3,12 +3,14 @@ package com.cyn.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chenyanan
  * Created by chenyanan on 2020/11/11
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 public class Order80 {
     public static void main(String[] args) {
         SpringApplication.run(Order80.class, args);
