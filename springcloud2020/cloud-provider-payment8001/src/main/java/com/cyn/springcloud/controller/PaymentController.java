ee04568210e997bdd3b6fbff6ce2f2313301c602
chenyn22577 <chenyn22577@hundsun.com>
2020-11-13 19:40:06 +0800

    add:service discovery and change default service instance name

--- springcloud2020/cloud-provider-payment8001/src/main/java/com/cyn/springcloud/controller/PaymentController.java
+++ springcloud2020/cloud-provider-payment8001/src/main/java/com/cyn/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import com.cyn.springcloud.service.PaymentService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author chenyanan
@@ -20,6 +25,12 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    /**
+     * 注意导入的包是springcloud的不是netflix
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     /**
      * 新增
@@ -53,5 +64,25 @@ public class PaymentController {
         } else {
             return new CommonResult<>(500, "query error id:" + id, null);
         }
+    }
+
+    /**
+     * 服务节点信息获取
+     *
+     * @return
+     */
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        // 所有服务
+        services.forEach(service -> {
+            log.info("****service:{}", service);
+        });
+        // 单个服务对应的实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        instances.forEach(instance -> {
+            log.info(instance.getServiceId() + "/" + instance.getHost() + "/" + instance.getPort() + "/" + instance.getUri());
+        });
+        return this.discoveryClient;
     }
 }
