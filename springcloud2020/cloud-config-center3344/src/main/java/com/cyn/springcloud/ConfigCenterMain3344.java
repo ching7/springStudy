05756a49fa8a5c5bafc9b25d10a90f90f31fa71f
chenyanan <775608598@qq.com>
2020-11-26 16:03:38 +0800

    add:spring ConfigCenter demo

---
+++ springcloud2020/cloud-config-center3344/src/main/java/com/cyn/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.cyn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author chenyanan
+ * Created by chenyanan on 2020/11/26
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
