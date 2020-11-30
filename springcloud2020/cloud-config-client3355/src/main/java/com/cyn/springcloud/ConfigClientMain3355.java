692c0bfd5c295727f9c4fcef7d251a769d3c191f
chenyanan <775608598@qq.com>
2020-11-30 15:25:12 +0800

    add:spring ConfigCenter manual refresh demo

---
+++ springcloud2020/cloud-config-client3355/src/main/java/com/cyn/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,15 @@
+package com.cyn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenyanan
+ * Created by chenyanan on 2020/11/27
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
