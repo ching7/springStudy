8d2de7b98c3398fcdafea2f5610fbd4b3e4385d0
ching <775608598@qq.com>
2021-07-13 10:53:23 +0800

    add:config test

--- springboot-unitTest/src/main/java/com/cyn/App.java
+++ springboot-unitTest/src/main/java/com/cyn/App.java
@@ -3,6 +3,7 @@ package com.cyn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -10,18 +11,15 @@ import org.springframework.context.annotation.PropertySources;
 /**
  * 文件描述
  *
- * @ProductName: Hundsun HEP
  * @ProjectName: springboot-unitTest
  * @Package: com.cyn
  * @Description: note
- * @Author: hspcadmin
+ * @Author: admin
  * @CreateDate: 2019/10/11 11:41
- * @UpdateUser: hspcadmin
  * @UpdateDate: 2019/10/11 11:41
  * @UpdateRemark: The modified content
  * @Version: 1.0
  * <p>
- * Copyright © 2019 Hundsun Technologies Inc. All Rights Reserved
  **/
 @SpringBootApplication
 //@PropertySources({
@@ -29,6 +27,11 @@ import org.springframework.context.annotation.PropertySources;
 //})
 public class App {
     public static void main(String[] args) {
+        //springboot默认的配置文件路径
+        // String addClassPath = "spring.config.location:classpath:/";
+        //自定义的配置文件路径
+        // String addClassPath = "spring.config.additional-location:classpath:/application-port.properties";
+        //new SpringApplicationBuilder(App.class).properties(addClassPath).build().run(args);
         SpringApplication.run(App.class, args);
     }
 }
