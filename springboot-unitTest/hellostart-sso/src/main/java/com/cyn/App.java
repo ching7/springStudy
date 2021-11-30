d10b8556d78ec3f2ee3c587ced9bbbff6a2feecd
ching <775608598@qq.com>
2021-11-30 16:37:59 +0800

    style:update directory

---
+++ springboot-unitTest/hellostart-sso/src/main/java/com/cyn/App.java
@@ -0,0 +1,16 @@
+package com.cyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: ynchen9
+ * @CreateTime: 2021-11-30
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
